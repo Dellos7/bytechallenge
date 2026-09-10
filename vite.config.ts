e12c0b662a0efb6b94f2af059efd304f81fbe86e
David López Castellote <david.lopez.castellote@gmail.com>
2026-09-10 09:29:57 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/bytechallenge/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
